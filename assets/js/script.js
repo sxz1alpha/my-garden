9a2cccc56af2b373f82ec82b24ce25714f34c9fb
Sir Ubu <thesirubu@gmail.com>
2021-04-21 20:20:17 -0600

    Added logic for dom display of results.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // DOM objects stored as variables
-var searchDisplayEl = $("#searchResults");
+var searchDisplayEl = $('#searchResults');
 
 // function to take search and get api response 
 var searchAPI = function(userSearch) {
     // update DOM to inform user application is searching
-    searchDisplayEl.text("Please wait....");
+    searchDisplayEl.text('Please wait....');
 
     // api address as variable
     var APIAddress = `https://openfarm.cc/api/v1/crops/?filter=${userSearch}`;
@@ -17,7 +17,7 @@ var searchAPI = function(userSearch) {
                 return response.json();
             // if error with response, update DOM
             } else {
-                searchDisplayEl.text("There was an error processing your request. Please alert the dev team.");
+                searchDisplayEl.text('There was an error processing your request. Please alert the dev team.');
                 return
             }
         })
@@ -32,15 +32,42 @@ var searchAPI = function(userSearch) {
 // function to display search results from API request to the DOM
 var displayResults = function(results) {
     // clear previous search/information from DOM
-    searchDisplayEl.text("");
+    searchDisplayEl.text('');
     // ensure there are results in the search
     if(results.data.length < 1) {
-        searchDisplayEl.text("You search yielded no crops... please try searching by a different name.");
+        searchDisplayEl.text('You search yielded no crops... please try searching by a different name.');
         return
     }
 
     for(var i = 0; i < results.data.length; i++) {
-        searchDisplayEl.append($("<div>").text(results.data[i].attributes.name));
+        // created and add content for card of each search result
+        var colEl = $('<a>').addClass('modal-trigger col s12 m4 l3').attr('href', `#${results.data[i].id}`).attr('plantId', results.data[i].id);
+
+        var cardEl = $('<div>').addClass('card');
+
+        var imageDiv = $('<div>').addClass('card-image');
+        if(results.data[i].attributes.main_image_path === "/assets/baren_field_square-4a827e5f09156962937eb100e4484f87e1e788f28a7c9daefe2a9297711a562a.jpg") {
+            var imgSrc = "./assets/images/baren_field_square-4a827e5f09156962937eb100e4484f87e1e788f28a7c9daefe2a9297711a562a.jpg";
+        } else {
+            var imgSrc = results.data[i].attributes.main_image_path;
+        }
+        var imageEl = $('<img>').attr('src', imgSrc).attr('style', "height:250px");
+        imageDiv.append(imageEl);
+        var spanEl = $('<span>').addClass('card-title').text(results.data[i].attributes.name);
+        imageDiv.append(spanEl);
+        
+        cardEl.append(imageDiv);
+        
+        // var contentDiv = $('<div>').addClass('card-content');
+
+        // var pEl = $('<p>').text(results.data[i].attributes.description);
+        // contentDiv.append(pEl);
+
+        // cardEl.append(contentDiv);
+        colEl.append(cardEl);
+
+        // add card to page
+        searchDisplayEl.append(colEl);
     }
 };
 
@@ -50,8 +77,11 @@ $("#searchBtn").click(function(event) {
     event.preventDefault();
 
     // store the user's entry
-    var userSearch = $("#userSearch").val();
+    var userSearch = $('#userSearch').val();
 
     // pass the user's entry into the searchAPI function
     searchAPI(userSearch);
+
+    // reset search input
+    $('#UserSearch').val('');
 });
